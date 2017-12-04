3a438bef81302d44023ef3bd19b0f746b84e6a62
ishiy <y.ishihara.1993@gmail.com>
2017-12-05 07:30:29 +0900

    Add irai.html

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const main = () => {
   const topPage = nunjucks.render('templates/top.html');
   fs.writeFileSync('docs/index.html',topPage);
+  const iraiPage = nunjucks.render('templates/irai.html');
+  fs.writeFileSync('docs/irai.html',iraiPage);
 };
 
 main();
