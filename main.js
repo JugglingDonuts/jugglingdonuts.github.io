3a9eebc73299941411264bd332d92ba6dc9f2046
ishiy <y.ishihara.1993@gmail.com>
2017-12-06 20:41:20 +0900

    Create member page

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const nunjucks = require('nunjucks');
+const toml = require('toml');
 const fs = require('fs');
 
 const main = () => {
   const topPage = nunjucks.render('templates/top.html');
   fs.writeFileSync('docs/index.html',topPage);
+
+  const years = [2016,2015];
+  const membersPage = nunjucks.render('templates/members.html', {years});
+  fs.writeFileSync('docs/members.html',membersPage);
+  years.forEach((year) => {
+    const memberData = fs.readFileSync(`members/${year}.toml`);
+    let members;
+    try {
+      members = toml.parse(memberData).members;
+    } catch(e) {
+      console.error(`Parse Error: ${e.line} in members/${year}.toml`);
+      process.exit(1);
+    }
+    const memberPage = nunjucks.render('templates/member.html', {year, members});
+    fs.writeFileSync(`docs/members/${year}.html`, memberPage);
+  });
+
   const iraiPage = nunjucks.render('templates/irai.html');
   fs.writeFileSync('docs/irai.html',iraiPage);
 };
