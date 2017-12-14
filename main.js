a6e8c6ef60aad0d44dd0dd3691137449c98cedac
ishiy <y.ishihara.1993@gmail.com>
2017-12-14 19:01:24 +0900

    Change docs to public

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 const nunjucks = require('nunjucks');
 const toml = require('toml');
 const fs = require('fs');
+const shell = require('shelljs');
 
 const main = () => {
-  const topPage = nunjucks.render('templates/top.html');
-  fs.writeFileSync('docs/index.html',topPage);
+  if (fs.existsSync('public')) {
+    shell.rm('-r', 'public');
+  }
+  shell.mkdir('public');
+  shell.mkdir('public/css');
+  shell.mkdir('public/members');
+
+  // copy img
+  const imgTable = {
+    top: 'JDL_2017.jpg',
+    members: 'NF_2017.jpg',
+    irai: 'NF_2017_koshihikari.jpg',
+  };
+  shell.cp('-r', 'img', 'public/img');
+
+  const topPage = nunjucks.render('templates/top.html', {imgTable});
+  fs.writeFileSync('public/index.html',topPage);
 
   const years = fs.readdirSync('members').sort().reverse().map(f => f.substr(0,4));
   const active = years.slice(0,4);
   const inactive = years.slice(4);
-  const membersPage = nunjucks.render('templates/members.html', {"years": active});
-  fs.writeFileSync('docs/members.html',membersPage);
+  const membersPage = nunjucks.render('templates/members.html', {"years": active, imgTable});
+  fs.writeFileSync('public/members.html',membersPage);
   active.forEach((year) => {
     const memberData = fs.readFileSync(`members/${year}.toml`);
     let members;
@@ -20,8 +36,8 @@ const main = () => {
       console.error(`Parse Error: ${e.line} in members/${year}.toml`);
       process.exit(1);
     }
-    const memberPage = nunjucks.render('templates/member.html', {year, members});
-    fs.writeFileSync(`docs/members/${year}.html`, memberPage);
+    const memberPage = nunjucks.render('templates/member.html', {year, members, imgTable});
+    fs.writeFileSync(`public/members/${year}.html`, memberPage);
   });
 
   const oldMember = inactive.map((year) => {
@@ -37,11 +53,11 @@ const main = () => {
     const isLast = year === '2007';
     return {year, members, isLast};
   });
-  const oldMemberPage = nunjucks.render('templates/old_member.html', {"data": oldMember});
-  fs.writeFileSync('docs/members/obog.html', oldMemberPage);
+  const oldMemberPage = nunjucks.render('templates/old_member.html', {"data": oldMember, imgTable});
+  fs.writeFileSync('public/members/obog.html', oldMemberPage);
 
-  const iraiPage = nunjucks.render('templates/irai.html');
-  fs.writeFileSync('docs/irai.html',iraiPage);
+  const iraiPage = nunjucks.render('templates/irai.html', {imgTable});
+  fs.writeFileSync('public/irai.html',iraiPage);
 };
 
 main();
